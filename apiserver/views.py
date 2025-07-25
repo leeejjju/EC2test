840bb880cb2246b0986fb8331cbf4fa349cca27d
leeejjju <leeejjju@gmail.com>
2025-07-25 14:57:28 +0900

    modifying test: v4

--- apiserver/views.py
+++ apiserver/views.py
@@ -4,5 +4,5 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result":"success", "data":[{"id":"leeejjju", "name":"JJLee(modified_v3)"}]}
+    data = {"result":"success", "data":[{"id":"leeejjju", "name":"JJLee(modified_v4)"}]}
     return Response(data, status=status.HTTP_200_OK)
